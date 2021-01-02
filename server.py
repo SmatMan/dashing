6fa3aa4c525008b5b5ab3d3beb5e420a2023ae80
SmatMan <gamingajun10@gmail.com>
2021-01-02 18:41:58 -0500

    forgot to push server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, render_template
 from config import owmkey # i made this config file myself, it just has my api keys
 
 app = Flask(__name__)
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", owmkey=owmkey)
-    print(owmkey)
+@app.route('/test')
+def test():
+    today = datetime.date.today()
+    future = datetime.date(2021,2,1)
+    diff = future - today
+    return jsonify(result=diff.days)
 
 if __name__ == "__main__":
     app.run(debug=True)
