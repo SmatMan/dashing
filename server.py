933012fba8030e3665bea24c8ac1ab4bdb2de14c
SmatMan <gamingajun10@gmail.com>
2021-01-02 18:46:07 -0500

    add datetime so timeuntil actually works

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from config import owmkey # i made this config file myself, it just has my api keys
+import datetime
+
 
 app = Flask(__name__)
     
